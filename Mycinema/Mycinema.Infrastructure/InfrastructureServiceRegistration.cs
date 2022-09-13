63a4a4dd21cc669b18ebe8652397a79b7e8a097a
Alejandro <alevelara@gmail.com>
2022-09-13 20:14:27 +0200

    Creating scheleton

---
+++ Mycinema/Mycinema.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Mycinema.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection RegisterConfiguration( this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
